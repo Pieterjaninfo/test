e760f8d5346fb1dfbe87be4dec0457963e0fd932
Pieter Jan <Pieter Jan@PieterJan>
2015-11-25 11:53:37 +0100

    Revert "changed hello.java and added helloworld.java"
    This reverts commit 7ab1b7af957e767f1b84339893cf759a911c585c.

--- githubtestproject/src/githubtestproject/Hello.java
+++ githubtestproject/src/githubtestproject/Hello.java
@@ -3,7 +3,7 @@ package githubtestproject;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World! JK not!");
+		System.out.println("Hello World!");
 
 	}
 
